91cff117e8c76a574151902fd289d6cae94e758f
xerfilstyx <xerfilstyx@gmail.com>
2021-02-02 00:26:59 +0900

    Exercise 1.33

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,7 +1,7 @@
 # pcost.py
 #
-# Exercise 1.32
-import csv
+# Exercise 1.33
+import csv, sys
 
 def portfolio_cost(filename):
     """A function to read file from filename(dir) and return the total cost"""
@@ -10,14 +10,19 @@ def portfolio_cost(filename):
         rows = csv.reader(file)
         header = next(rows)     # skip header string
         for row in rows:
-            print(row)
             # now row is like ['AA', '100', '32.20' thanks to csv module]
             try:
                 total += int(row[1]) * float(row[2])
             except ValueError:
                 print('Invalid data. Please check your filename.')
+                break
     
     return total
 
-cost = portfolio_cost('Data/portfolio.csv')
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
 print('Total cost:', cost)
