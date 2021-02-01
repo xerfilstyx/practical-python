c0ddea4f8e11a68322659cb3a4fb977c6608f887
xerfilstyx <xerfilstyx@gmail.com>
2021-02-02 00:00:38 +0900

    Exercise 1.30

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,13 +1,17 @@
 # pcost.py
 #
-# Exercise 1.27
-total = 0
+# Exercise 1.30
+def portfolio_cost(filename):
+    """A function to read file from filename(dir) and return the total cost"""
+    total = 0
+    with open(filename, 'rt') as file:
+        header = next(file)     # skip header string
+        for line in file:
+            row = line.split(',')
+            # now row is like ['"AA"', '100', '32.20\n']
+            total += int(row[1]) * float(row[2].strip('\n'))
+    
+    return total
 
-with open('Data\portfolio.csv', 'rt') as file:
-    header = next(file)     # skip header string
-    for line in file:
-        row = line.split(',')
-        # now row is like ['"AA"', '100', '32.20\n']
-        total += int(row[1]) * float(row[2].strip('\n'))
-
-print('Total cost', total)
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total cost:', cost)
