84b38ffd32865a84c91b1cba655e63398461a9d1
xerfilstyx <xerfilstyx@gmail.com>
2021-02-01 23:44:46 +0900

    Exercise 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,13 @@
 # pcost.py
 #
 # Exercise 1.27
+total = 0
+
+with open('Data\portfolio.csv', 'rt') as file:
+    header = next(file)     # skip header string
+    for line in file:
+        row = line.split(',')
+        # now row is like ['"AA"', '100', '32.20\n']
+        total += int(row[1]) * float(row[2].strip('\n'))
+
+print('Total cost', total)
