97f804b2054dbe981e5ea66412ea614470d402e9
xerfilstyx <xerfilstyx@gmail.com>
2021-02-05 12:06:48 +0900

    Exercise 4.9

--- Work/stock.py
+++ Work/stock.py
@@ -1,11 +1,14 @@
 # stock.py
 #
-# Exercise 4.2
+# Exercise 4.9
 class Stock:
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
+
+    def __repr__(self):
+        return f'Stock({self.name!r}, {self.shares!r}, {self.price!r})'
     
     def cost(self):
         return self.shares * self.price
