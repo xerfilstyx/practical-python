24b5032915939c58f577f1be8dca198c001ac4c5
xerfilstyx <xerfilstyx@gmail.com>
2021-02-06 16:26:51 +0900

    Exercise 6.12

--- Work/ticker.py
+++ Work/ticker.py
@@ -1,8 +1,8 @@
 # ticker.py
 #
-# Exercise 6.11
+# Exercise 6.12
 from follow import follow
-import csv
+import csv, report
 
 def select_columns(rows, indices):
     for row in rows:
@@ -28,8 +28,24 @@ def filter_symbols(rows, names):
         if row['name'] in names:
             yield row
 
-if __name__ == '__main__':
-    lines = follow('Data/stocklog.csv')
-    rows = parse_stock_data(lines)
+def ticker(portfile, logfile, fmt = 'txt'):
+    """
+    포트폴리오 파일(portfile), 로그 파일(logfile), 포맷(fmt) 형식을 이용해 실시간 주식 티커를 생성하는 함수
+    """
+    portfolio = report.read_portfolio(portfile)
+    rows = parse_stock_data(follow(logfile))
+    rows = filter_symbols(rows, portfolio)
+    formatter = report.tableformat.create_formatter(fmt)
+    formatter.headings(['Name', 'Price', 'Change'])
     for row in rows:
-        print(row)
+        rowdata = [row['name'], str(row['price']), str(row['change'])]
+        formatter.row(rowdata)
+
+def main(args):
+    if len(args) != 4:
+        raise SystemExit('Usage: ticker portfoliofile logfile fmt')
+    ticker(args[1], args[2], args[3])
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv)
