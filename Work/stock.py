b292c3d1dee16c796ad014b4305b6546d0c5bc8a
xerfilstyx <xerfilstyx@gmail.com>
2021-02-06 18:52:33 +0900

    Exercise 7.7

--- Work/stock.py
+++ Work/stock.py
@@ -1,11 +1,17 @@
 # stock.py
 #
-# Exercise 5.8
+# Exercise 7.7
+from typedproperty import typedproperty
+
 class Stock:
     """
     An instance of a stock holding consisting of name, shares, and price.
     """
-    __slots__ = ['name', '_shares', 'price']
+
+    name = typedproperty('name', str)
+    shares = typedproperty('shares', int)
+    price = typedproperty('price', float)
+
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
@@ -14,16 +20,6 @@ class Stock:
     def __repr__(self):
         return f'Stock({self.name!r}, {self.shares!r}, {self.price!r})'
     
-    @property
-    def shares(self):
-        return self._shares
-    
-    @shares.setter
-    def shares(self, value):
-        if not isinstance(value, int):
-            raise TypeError("Expected an integer.")
-        self._shares = value
-
     @property
     def cost(self):
         return self.shares * self.price
