9ccbff1b9d824a9492d5877188ba32f3bde9abac
xerfilstyx <xerfilstyx@gmail.com>
2021-02-08 12:08:23 +0900

    Exercise 9.3

---
+++ Work/porty-app/setup.py
@@ -0,0 +1,13 @@
+# setup.py
+#
+# Exercise 9.3
+import setuptools
+
+setuptools.setup(
+    name = 'porty',
+    version = '0.0.1',
+    author = 'xerfilstyx',
+    author_email = 'xerfilstyx@example.com',
+    description = 'Practical Python Code',
+    packages = setuptools.find_packages(),
+)
